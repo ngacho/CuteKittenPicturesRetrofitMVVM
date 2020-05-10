b9efa021cb76d9ed49e8f2ac17f106ef092b914a
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-10 15:53:30 +0300

    refactored code: changed view pager adapter according to suggestions by Gabor Varadi

--- app/src/main/java/com/brocodes/catspics/view/utils/ViewPagerAdapter.kt
+++ app/src/main/java/com/brocodes/catspics/view/utils/ViewPagerAdapter.kt
@@ -2,13 +2,11 @@ package com.brocodes.catspics.view.utils
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.brocodes.catspics.constants.PawIdentifiers
-import com.brocodes.catspics.view.ui.DoggoFragment
-import com.brocodes.catspics.view.ui.KittieFragment
+import com.brocodes.catspics.view.ui.puppies.DoggoFragment
+import com.brocodes.catspics.view.ui.kittens.KittieFragment
 import java.lang.IllegalStateException
-import java.util.*
 
 @Suppress("deprecation")
 class ViewPagerAdapter(supportFragmentManager: FragmentManager): FragmentStatePagerAdapter(supportFragmentManager) {
@@ -23,8 +21,8 @@ class ViewPagerAdapter(supportFragmentManager: FragmentManager): FragmentStatePa
     override fun getCount() = 2
 
     override fun getPageTitle(position: Int): CharSequence = when(position){
-        0 -> PawIdentifiers.DOGGO
-        1 -> PawIdentifiers.KITTIE
+        0 -> PawIdentifiers.KITTIE
+        1 -> PawIdentifiers.DOGGO
         else -> throw IllegalStateException("Unexpected position $position")
     }
 
