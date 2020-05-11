40027726b84085a4fd4bac84243c2cc3d0b6b292
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-12 01:55:34 +0300

    feature added: Kotlin Coroutines Implemented

--- app/src/main/java/com/brocodes/catspics/data/PixabayMethods.kt
+++ app/src/main/java/com/brocodes/catspics/data/PixabayMethods.kt
@@ -1,21 +1,22 @@
 package com.brocodes.catspics.data
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PixabayMethods {
 
     @GET("/api/")
-    fun getPaws(
+    suspend fun getPaws(
         @Query("key") key: String = "16180650-cc5b3804ae28c4b936b61007f",
         @Query("q") queryValue: String
-    ): Call<PixabayResponse>
+    ): Response<PixabayResponse>
 
     @GET("/api/")
-    fun loadMorePaws(
+    suspend fun loadMorePaws(
         @Query("key") key: String = "16180650-cc5b3804ae28c4b936b61007f",
         @Query("q") queryValue: String,
         @Query("page") resultPage : Int
-    ) : Call<PixabayResponse>
+    ) : Response<PixabayResponse>
 }
