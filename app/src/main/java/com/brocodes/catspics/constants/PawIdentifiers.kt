247228a0a61443f9eaf8a1534d4d2ec1be12a3df
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-15 00:44:13 +0300

    feature added: fragments survive configuration changes

--- app/src/main/java/com/brocodes/catspics/constants/PawIdentifiers.kt
+++ app/src/main/java/com/brocodes/catspics/constants/PawIdentifiers.kt
@@ -6,4 +6,5 @@ object PawIdentifiers{
     const val PUPPIES = "Puppies"
     const val KITTIE = "KITTIE"
     const val DOGGO = "DOGGO"
+    const val API_KEY = "16180650-cc5b3804ae28c4b936b61007f"
 }
